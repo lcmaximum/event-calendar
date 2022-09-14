827a0a148cf8320485d672db322567ebb04eae07
LC Clark <laura.mc.clark@gmail>
2022-09-14 17:59:59 -0400

    adds nav bar

--- models/event.js
+++ models/event.js
@@ -4,6 +4,7 @@ const Schema= mongoose.Schema;
 const eventSchema = new Schema({
     title: String,
     date: Date,
+    dateString: String,
     evHour: String,
     evMinute: String,
     evAMPM: String
